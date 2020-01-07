8db2b20516ce691e9b51583cf739b622b4f2effe
Wael Kdouh <waekdo@microsoft.com>
2020-01-07 14:58:33 -0500

    Initial commit

---
+++ BlazorInExistingMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BlazorInExistingMVCApp.Models;
+
+namespace BlazorInExistingMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // TODO: The model to be passed to the view and ultimately to the blazor component
+            PersonViewModel aperson = new PersonViewModel()
+            {
+                FirstName = "Wael",
+                LastName = "kdouh"
+            };
+
+            return View(aperson);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BlazorInExistingMVCApp/Models/PersonViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorInExistingMVCApp.Models
+{
+    public class PersonViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
